eda8dd92ce1b28a32ec96a04c1dcc775d1b70905
Xander White <xaw8720@g.rit.edu>
2024-10-27 15:56:46 -0400

    Fixed bug associated with being prompted for a game input twice

--- Working/Main.py
+++ Working/Main.py
@@ -3,23 +3,22 @@ from Word_Guess import main as word_guess
 from Extras import handle_value_error, clear_screen
 
 #Check if the choice is in the menu
-def check_menu_choice(menu=[], choice=None):
+def check_menu_choice(menu, choice=None):
     #Check that parameters are valid
-    if menu == []:
-        return main()
     if choice == None:
         choice = input()
     #Check if the choice is in the menu as an integer
-    if choice in range(len(menu)):
-        return menu[choice]
+    try:
+        if int(choice) in range(len(menu)):
+            return menu[int(choice)]
     #check if the choice is in the menu as a string
-    elif choice.strip().lower().replace(" ", "_") in [item.strip().lower().replace(" ", "_") for item in menu]:
-        return choice
-    #If the choice is not in the menu, ask for a new choice, suggesting the user inputs an intiger
-    else:
-        print("Invalid choice.")
-        choice = handle_value_error()
-        return check_menu_choice(menu, choice=choice)
+    except ValueError:
+        if choice.strip().lower().replace(" ", "_") in [item.strip().lower().replace(" ", "_") for item in menu]:
+            return choice
+    #If the choice is not in the menu, ask for a new choice, suggesting the user inputs an integer
+    print("Invalid choice.")
+    choice = handle_value_error()
+    return check_menu_choice(menu, choice=choice)
 
 #Main function
 def main():
@@ -30,7 +29,7 @@ def main():
     print("Which game would you like to play?\n")
     print("\n".join([f"{index} : {menu[index]}" for index in range(len(menu))]))
     #Checks the choice for the corresponding name or index in menu[]
-    choice_name = (check_menu_choice(menu).strip().lower()).replace(" ", "_")
+    choice_name = check_menu_choice(menu).strip().lower().replace(" ", "_")
     #Evaluates the choice to call the corresponding game function
     eval((choice_name + '()'))
 
