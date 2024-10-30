74ecb979d8fa783814463d82612fcd3dc4a432fd
Xander White <xaw8720@g.rit.edu>
2024-10-30 12:36:54 -0400

    Streamlined code

--- Working/Main.py
+++ Working/Main.py
@@ -3,11 +3,9 @@ from Word_Guess import main as word_guess
 from Extras import handle_value_error, clear_screen
 
 #Check if the choice is in the menu
-def check_menu_choice(menu, choice=None):
+def check_menu_choice(menu, prompt=''):
     while True:
-        #Check that parameters are valid
-        if choice == None:
-            choice = input()
+        choice = input(prompt)
         #Check if the choice is in the menu as an integer
         try:
             if int(choice) in range(len(menu)):
@@ -17,19 +15,16 @@ def check_menu_choice(menu, choice=None):
             if choice.strip().lower().replace(" ", "_") in [item.strip().lower().replace(" ", "_") for item in menu]:
                 return choice
         #If the choice is not in the menu, ask for a new choice, suggesting the user inputs an integer
-        choice = handle_value_error("Invalid choice.")
+        clear_screen("Invalid choice.")
 
 #Main function
 def main():
     while True:
         #Defines the games in the menu as a list
         menu = ["Quit", "Nim", "Word Guess"]
-        #Asks the user to choose a game, printing the games in the menu
-        clear_screen("Which game would you like to play?\n\n" + "\n".join([f"{index} : {menu[index]}" for index in range(len(menu))]) + "\n")
-        #Checks the choice for the corresponding name or index in menu[]
-        choice_name = check_menu_choice(menu).strip().lower().replace(" ", "_")
-        #Evaluates the choice to call the corresponding game function
-        eval((choice_name + '()'))
+        clear_screen()
+        #Gets a proper input and runs the menu item associated with it
+        eval((check_menu_choice(menu, "Which game would you like to play?\n\n" + "\n".join([f"{index} : {menu[index]}" for index in range(len(menu))]) + "\n\n").strip().lower().replace(" ", "_") + '()'))
 
 if __name__ == "__main__":
     main()
