a4b8059c473351386bad055b9ef06982f5b085c3
Xander White <xaw8720@g.rit.edu>
2024-11-07 19:31:26 -0500

    Improved movement system, issues printing the screen

--- Snake.py
+++ Snake.py
@@ -9,35 +9,49 @@ def main(active_user='Guest'):
         max_apples = 3
         apples = []
 
-        map_width = handle_value_error("Map width: ")
-        map_height = handle_value_error("Map height: ")
+        # map_width = handle_value_error("Map width: ")
+        # map_height = handle_value_error("Map height: ")
+        map_width = 20
+        map_height = 20
 
         game_map = [['██' for _ in range(map_width)] for _ in range(map_height)]
+        previous_map = [['██' for _ in range(map_width)] for _ in range(map_height)]
 
         player_position = [(random.randint(0, map_height-1), random.randint(0, map_width-1))]
         for i in range(max_apples):
             while True:
-                apple_poition = (random.randint(0, map_height-1), random.randint(0, map_width-1))
-                if apple_poition != player_position and apple_poition not in apples:
-                    apples.append(apple_poition)
+                apple_position = (random.randint(0, map_height-1), random.randint(0, map_width-1))
+                if apple_position != player_position[0] and apple_position not in apples:
+                    apples.append(apple_position)
                     break
 
         game_map[player_position[0][0]][player_position[0][1]] = '  '
+        #Print initial game map
+        for apple in apples:
+            game_map[apple[0]][apple[1]] = '🍎'
+        for y in range(map_height):
+            for x in range(map_width):
+                if game_map[y][x] != previous_map[y][x]:
+                    print(f"\033[{y+1};{x*2+1}H{game_map[y][x]}", end='')
+        previous_map = [row[:] for row in game_map]
 
         while True:
-            clear_screen()
             if player_position[0] in apples:
                 snake_length += 1
                 apples.remove(player_position[0])
                 while True:
-                    apple_poition = (random.randint(0, map_height-1), random.randint(0, map_width-1))
-                    if apple_poition != player_position and apple_poition not in apples:
-                        apples.append(apple_poition)
+                    apple_position = (random.randint(0, map_height-1), random.randint(0, map_width-1))
+                    if apple_position != player_position[0] and apple_position not in apples:
+                        apples.append(apple_position)
                         break
+
             for apple in apples:
                 game_map[apple[0]][apple[1]] = '🍎'
-            for row in game_map:
-                print(''.join(row))
+            for y in range(map_height):
+                for x in range(map_width):
+                    if game_map[y][x] != previous_map[y][x]:
+                        print(f"\033[{y+1};{x*2+1}H{game_map[y][x]}", end='')
+            previous_map = [row[:] for row in game_map]
 
             if keyboard.is_pressed('w') or keyboard.is_pressed('up') and movement != 'down':
                 movement = 'up'
@@ -50,38 +64,35 @@ def main(active_user='Guest'):
 
             if movement == 'up':
                 if player_position[0][0]-1 in range(map_height):
-                    game_map[player_position[0][0]][player_position[0][1]] = '██'
-                    player_position[0] = (player_position[0][0]-1, player_position[0][1])
-                    game_map[player_position[0][0]][player_position[0][1]] = '  '
-                    time.sleep(0.2)
+                    new_head = (player_position[0][0]-1, player_position[0][1])
                 else:
                      break
             elif movement == 'down':
                 if player_position[0][0]+1 in range(map_height):
-                    game_map[player_position[0][0]][player_position[0][1]] = '██'
-                    player_position[0] = (player_position[0][0]+1, player_position[0][1])
-                    game_map[player_position[0][0]][player_position[0][1]] = '  '
-                    time.sleep(0.2)
+                    new_head = (player_position[0][0]+1, player_position[0][1])
                 else:
                     break
             elif movement == 'left':
                 if player_position[0][1]-1 in range(map_width):
-                    game_map[player_position[0][0]][player_position[0][1]] = '██'
-                    player_position[0] = (player_position[0][0], player_position[0][1]-1)
-                    game_map[player_position[0][0]][player_position[0][1]] = '  '
-                    time.sleep(0.2)
+                    new_head = (player_position[0][0], player_position[0][1]-1)
                 else:
                     break
             elif movement == 'right':
                 if player_position[0][1]+1 in range(map_width):
-                    game_map[player_position[0][0]][player_position[0][1]] = '██'
-                    player_position[0] = (player_position[0][0], player_position[0][1]+1)
-                    game_map[player_position[0][0]][player_position[0][1]] = '  '
-                    time.sleep(0.2)
+                    new_head = (player_position[0][0], player_position[0][1]+1)
                 else:
                     break
             else:
-                time.sleep(0.2)
+                continue
+
+            player_position.insert(0, new_head)
+            if len(player_position) > snake_length:
+                tail = player_position.pop()
+                game_map[tail[0]][tail[1]] = '██'
+
+            game_map[player_position[0][0]][player_position[0][1]] = '  '
+
+            time.sleep(0.2)
         #Try again?
         clear_screen("Game Over\n")
         response = yes_or_no("Would you like to play again? (Y/N)\n")
