99b525a6ce2ccb93f216194fc2ccfe3f7f88d909
Xander White <xaw8720@g.rit.edu>
2024-11-12 12:57:52 -0500

    Updated screen refresh

--- Snake.py
+++ Snake.py
@@ -15,10 +15,10 @@ def main(active_user='Guest'):
         map_height = 20
 
         game_map = [['██' for _ in range(map_width)] for _ in range(map_height)]
-        previous_map = [['██' for _ in range(map_width)] for _ in range(map_height)]
+        previous_map = [['  ' for _ in range(map_width)] for _ in range(map_height)]
 
         player_position = [(random.randint(0, map_height-1), random.randint(0, map_width-1))]
-        for i in range(max_apples):
+        for _ in range(max_apples):
             while True:
                 apple_position = (random.randint(0, map_height-1), random.randint(0, map_width-1))
                 if apple_position != player_position[0] and apple_position not in apples:
@@ -26,15 +26,9 @@ def main(active_user='Guest'):
                     break
 
         game_map[player_position[0][0]][player_position[0][1]] = '  '
-        #Print initial game map
-        for apple in apples:
-            game_map[apple[0]][apple[1]] = '🍎'
-        for y in range(map_height):
-            for x in range(map_width):
-                if game_map[y][x] != previous_map[y][x]:
-                    print(f"\033[{y+1};{x*2+1}H{game_map[y][x]}", end='')
-        previous_map = [row[:] for row in game_map]
 
+        for row in game_map:
+            print("".join(row))
         while True:
             if player_position[0] in apples:
                 snake_length += 1
@@ -66,7 +60,7 @@ def main(active_user='Guest'):
                 if player_position[0][0]-1 in range(map_height):
                     new_head = (player_position[0][0]-1, player_position[0][1])
                 else:
-                     break
+                    break
             elif movement == 'down':
                 if player_position[0][0]+1 in range(map_height):
                     new_head = (player_position[0][0]+1, player_position[0][1])
@@ -92,8 +86,15 @@ def main(active_user='Guest'):
 
             game_map[player_position[0][0]][player_position[0][1]] = '  '
 
+            for y in range(map_height):
+                for x in range(map_width):
+                    if game_map[y][x] != previous_map[y][x]:
+                        print(f"\033[{y+1};{x*2+1}H{game_map[y][x]}", end='', flush=True)
+            previous_map = [row[:] for row in game_map]
+
             time.sleep(0.2)
-        #Try again?
+        
+        # Try again?
         clear_screen("Game Over\n")
         response = yes_or_no("Would you like to play again? (Y/N)\n")
         if response == "n":
@@ -103,7 +104,3 @@ def main(active_user='Guest'):
 
 if __name__ == "__main__":
     main()
-
-'''
-Modify this code to make snake later
-'''
