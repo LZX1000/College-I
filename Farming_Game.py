55d073d3f2ef5d89c80feb882eda141c8ba386b6
LZX1000 <xaw8720@g.rit.edu>
2024-11-24 17:29:00 -0500

    Movement version 1

--- Farming_Game.py
+++ Farming_Game.py
@@ -26,41 +26,40 @@ def main(active_user='Guest'):
             previous_map = [row[:] for row in game_map]
             print(f"\33[{map_height+3};0H", end='', flush=True)
             # Check for movement inputs
+            old_position = None
             while True:
-                old_position = None
                 if keyboard.is_pressed('w') or keyboard.is_pressed('up'):
                     if end_time-start_time >= movement_time or start_time is None:
                         start_time = time.monotonic()
-                        if player_position[0]-1 in range(map_height) and (player_position[0]-1, player_position[1]) not in player_position:
+                        if player_position[0]-1 in range(map_height) and old_position[0] != player_position[0]+1:
                             old_position = player_position
                             player_position = (player_position[0]-1, player_position[1])
-                            break
                 elif keyboard.is_pressed('s') or keyboard.is_pressed('down'):
                     if end_time-start_time >= movement_time or start_time is None:
                         start_time = time.monotonic()
-                        if player_position[0]+1 in range(map_height) and (player_position[0]+1, player_position[1]) not in player_position:
+                        if player_position[0]+1 in range(map_height) and old_position[0] != player_position[0]-1:
                             old_position = player_position
                             player_position = (player_position[0]+1, player_position[1])
-                            break
                 elif keyboard.is_pressed('a') or keyboard.is_pressed('left'):
                     if end_time-start_time >= movement_time or start_time is None:
                         start_time = time.monotonic()
-                        if player_position[1]-1 in range(map_width) and (player_position[0], player_position[1]-1) not in player_position:
+                        if player_position[1]-1 in range(map_width) and old_position[1] != player_position[1]+1:
                             old_position = player_position
                             player_position = (player_position[0], player_position[1]-1)
-                            break
                 elif keyboard.is_pressed('d') or keyboard.is_pressed('right'):
                     if end_time-start_time >= movement_time or start_time is None:
                         start_time = time.monotonic()
-                        if player_position[1]+1 in range(map_width) and (player_position[0], player_position[1]+1) not in player_position:
+                        if player_position[1]+1 in range(map_width) and old_position[1] != player_position[1]-1:
                             old_position = player_position
                             player_position = (player_position[0], player_position[1]+1)
-                            break
                 # Handle movement if enough time has passed
                 end_time = time.monotonic()
+                if end_time-start_time >= movement_time or start_time is None:
+                    break
             # Update player position and handle trail
-            game_map[old_position[0]][old_position[1]] = '\033[47m  \033[0m'
-            game_map[player_position[0]][player_position[1]] = '\033[93m██\033[0m'
+            if old_position is not None and old_position != player_position:
+                game_map[old_position[0]][old_position[1]] = '\033[47m  \033[0m'
+                game_map[player_position[0]][player_position[1]] = '\033[93m██\033[0m'
         # Game over
         clear_screen("Game Over")
         # Play again
