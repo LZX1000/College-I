188f09b06a7501bcd68369da34d3d9c7961b3d84
Xander White <xaw8720@g.rit.edu>
2024-12-02 16:11:59 -0500

    partial commit

--- helper_module.py
+++ helper_module.py
@@ -1,4 +1,7 @@
-import socket, urllib.request
+import os, socket, urllib.request, time
+
+def get_current_time():
+    return time.strftime("%H:%M:%S", time.localtime())
 
 def helper_1():
     hostname = socket.gethostname()
@@ -9,9 +12,24 @@ def helper_2():
     with urllib.request.urlopen('https://api.ipify.org') as response:
         return response.read().decode('utf-8')
 
+def helper_3():
+    return {
+        "current_time": get_current_time(),
+        "local": helper_1(),
+        "public": helper_2(),
+        "platform": os.name,
+        "hostname": socket.gethostname()
+    }
+
 def main():
-    print("Local: " + helper_1())
-    print("Public: " + helper_2())
+    stats = helper_3()
+
+    try:
+        with open("interesting_stats.txt", "a") as file:
+            file.write(", ".join([f"{key}: {value}" for key, value in stats])  + "\n\n")
+    except FileNotFoundError:
+        with open("interesting_stats.txt", "w") as file:
+            file.write(", ".join([f"{key}: {value}" for key, value in stats]) + "\n\n")
 
 if __name__ == "__main__":
     main()
