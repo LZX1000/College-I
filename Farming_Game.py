b19c584ed2f3ecdf4afc1e1a0fb91ff3272c099c
LZX1000 <xaw8720@g.rit.edu>
2024-12-14 03:54:18 -0500

    partial commit

--- Farming_Game.py
+++ Farming_Game.py
@@ -1,5 +1,5 @@
 import random, time, keyboard
-from Extras import clear_screen, yes_or_no
+from Extras import clear_screen, multiple_choice
 
 def main(active_user='Guest'):
     map_width = 16
@@ -18,7 +18,7 @@ def main(active_user='Guest'):
         # Game loop
         while True:
             game_map[player_position[0]][player_position[1]] = '\033[93m██\033[0m'
-            print("\033[1;1HPoints: {points}\n")
+            print("\033[?25l\033[1;1HPoints: {points}\n")
             # Display game map
             for y in range(map_height):
                 for x in range(map_width):
@@ -31,16 +31,16 @@ def main(active_user='Guest'):
             x_movement = 0
             while True:
                 if keyboard.is_pressed('w') or keyboard.is_pressed('up'):
-                    y_movement = -1
-                elif keyboard.is_pressed('s') or keyboard.is_pressed('down'):
                     y_movement = 1
+                elif keyboard.is_pressed('s') or keyboard.is_pressed('down'):
+                    y_movement = -1
                 if keyboard.is_pressed('a') or keyboard.is_pressed('left'):
                     x_movement = -1
                 elif keyboard.is_pressed('d') or keyboard.is_pressed('right'):
                     x_movement = 1
                 if keyboard.is_pressed('esc'):
                     clear_screen()
-                    pause_response = yes_or_no("Are you sure you want to quit? (Y/N)\n\n")
+                    pause_response = multiple_choice("Are you sure you want to quit?")
                     clear_screen()
                     if pause_response == "y":
                         playing = False
@@ -59,24 +59,9 @@ def main(active_user='Guest'):
                 color = random.choice(['\033[41m  \033[0m', '\033[42m  \033[0m', '\033[43m  \033[0m', '\033[44m  \033[0m', '\033[45m  \033[0m', '\033[46m  \033[0m'])
                 game_map[old_position[0]][old_position[1]] = color
                 saved_coordinates[old_position] = color
-                for y in range(map_height):
-                    for x in range(map_width):
-                        saved_coordinates[(y, x)] = game_map[y][x]
-                        new_y = y + y_movement
-                        new_x = x + x_movement
-                        if new_y < 0:
-                            new_y = 0
-                        elif new_y >= map_height:
-                            new_y = map_height-1
-                        if new_x < 0:
-                            new_x = 0
-                        elif new_x >= map_width:
-                            new_x = map_width-1
-                        game_map[y][x] = saved_coordinates.get((new_y, new_x), previous_map[new_y][new_x] if previous_map[new_y][new_x] == '\033[47m  \033[0m' else '\033[47m  \033[0m')
-        # Game over
-        clear_screen("Game Over")
+                
         # Play again
-        response = yes_or_no("\nWould you like to play again? (Y/N)\n")
+        response = multiple_choice("Game Over\n\nWould you like to play again?")
         if response == "n":
             return 'Snake', active_user, None
         if response == "y":
