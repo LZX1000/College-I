ae1427e9a2c3f2f1be6ba6ca9933ad1589707cec
Xander White <xaw8720@g.rit.edu>
2025-01-23 00:23:02 -0500

    Added broken money and ball cost

---
+++ Incremental_Incremental.py
@@ -0,0 +1,102 @@
+import pygame
+import random
+import time
+import ctypes
+
+def main():
+    class Ball:
+        def __init__(self):
+            rarity = random.randint(1, 100)
+            if rarity <= 50:
+                self.rarity = "Common"
+                self.money_value = 1
+            elif rarity <= 80:
+                self.rarity = "Rare"
+                self.money_value = 5
+            elif rarity <= 95:
+                self.rarity = "Epic"
+                self.money_value = 25
+            else:
+                self.rarity = "Legendary"
+                self.money_value = 100
+            
+            self.time = time.monotonic()
+        
+        def check_for_payount(self):
+            if time.monotonic() - self.time >= 5:
+                self.time = time.monotonic()
+                return self.money_value
+            return 0
+        
+    # Initialize
+    pygame.init()
+    pygame.font.init()
+    font = pygame.font.SysFont("Arial", 20)
+    pygame.display.set_caption("CollegeI")
+    clock = pygame.time.Clock()
+
+    # Display
+    # # Resolutions
+    internal_width = 640
+    internal_height = 360
+    ctypes.windll.user32.SetProcessDPIAware()
+    screen_size = (1920, 1080)
+    pixelation_factor = 2
+    # # Surfaces
+    internal_surface = pygame.Surface((internal_width, internal_height)) # For rendering
+    screen = pygame.display.set_mode(screen_size) # pygame.FULLSCREEN
+
+    running = True
+    new = True # Change when adding saving
+    max_fps = 60
+    dt = 0
+
+    while running:
+        # Exit game
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        if new:
+            player_money = 100
+            player_money_per_click = 1
+            new_ball_cost = 0
+            balls = []
+            unresolved_balls = []
+            new = False
+        
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_ESCAPE]:
+            running = False
+        elif keys[pygame.K_RETURN]:
+            if player_money >= new_ball_cost:
+                player_money -= new_ball_cost
+                new_ball_cost = len(balls) ** 4 + 10
+                unresolved_balls.append(0)
+        
+        if len(unresolved_balls) > 0:
+            for _ in unresolved_balls:
+                new_ball = Ball()
+                balls.append(new_ball)
+
+        for ball in balls:
+            player_money += ball.check_for_payount()
+
+        # Render in 640x360
+        internal_surface.fill((180, 180, 180)) # Sub-background
+        # background.game_update(internal_surface)
+        money_text_surface = font.render(f"Money :  {player_money}", False, (0, 0, 0)) # For text
+        ball_cost_text_surface = font.render(f"Ball Cost : {new_ball_cost}", False, (0, 0, 0)) # For text
+        internal_surface.blit(money_text_surface, (0, 0))
+        internal_surface.blit(ball_cost_text_surface, (0, 20))
+
+        # Upscale to 1920x1080
+        scaled_surface = pygame.transform.scale(internal_surface, screen_size)
+        screen.blit(scaled_surface, (0, 0))
+
+        pygame.display.flip()
+        # Tick Speed
+        dt = clock.tick(max_fps) / 1000
+
+if __name__ == "__main__":
+    main()
